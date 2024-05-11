2fc86332db6482a768d878aef56dc5d09a7f24ff
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-11 11:46:45 +0530

    sidebar done

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { navLinks } from "@/constants";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -25,32 +25,65 @@ const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname;
                 return (
                   <li
                     key={link.route}
                     className={`sidebar-nav_element group ${
-                        isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                      }`}
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
                   >
                     <Link className="sidebar-link" href={link.route}>
-                    <Image src={link.icon} alt="logo" width={24} height={24} className={`${isActive && 'brightness-200'}`}/>
-                    {link.label}
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
                     </Link>
                   </li>
                 );
               })}
-
-<li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl='/' showName />
+            </ul>
+            <ul className="sidebar-nav_elements">
+            {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl="/" showName />
               </li>
             </ul>
           </SignedIn>
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in" >Login</Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </nav>
