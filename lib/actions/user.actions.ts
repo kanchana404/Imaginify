ec1c9aacbe4b5deb7533024a6cac87491f556266
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-13 20:52:12 +0530

    pleaseeee

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,7 +12,7 @@ export async function createUser(user: CreateUserParams) {
     await connectToDatabase();
 
     const newUser = await User.create(user);
-    console.log("User created:", JSON.stringify(newUser)); 
+
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
