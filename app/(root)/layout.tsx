5f16c47ba7482ede97cde102470c71bcdfa1c1a5
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-26 02:30:04 +0530

    chore: Add MobileNav component to root layout and update Sidebar navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
@@ -5,7 +6,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
-      {/* MobileNavigation */}
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
