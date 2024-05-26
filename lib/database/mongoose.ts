eef9285bca8727ec1f3661af130b8fca12a7e6d8
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-26 15:37:44 +0530

    wwwww

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -6,3 +6,24 @@ interface MongooseConnection {
     conn:Mongoose | null;
     promise: Promise<Mongoose> | null;
 }
+
+let cached: MongooseConnection = (global as any).mongoose;
+if(!cached){
+    cached = (global as any).mongoose = {
+        conn:null, promise:null
+    };
+}
+
+export const connectToDatabase = async () => {
+    if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URL) throw new Error('MONGODB_URL is not defined');
+
+    cached.promise =
+     cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName:'imaginify', bufferCommands: false
+    })
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
